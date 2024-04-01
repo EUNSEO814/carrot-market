383fdba92919b66c2bf032a8463d8edd068c8eda
EUNSEO814 <eunseo814@gmail.com>
2024-04-02 05:11:24 +0900

    Code Challenge

---
+++ app/learnTailwind/CodeChallenge.tsx
@@ -0,0 +1,70 @@
+const CodeChallenge = () => {
+  return (
+    <div className="min-h-screen overflow-hidden bg-gray-50 py-24 ">
+      <div className="max-w-sm bg-[#FEE502] my-0 mx-auto py-4 px-4 rounded-[40px]">
+        <div className="pt-80 font-bold">Select Destination</div>
+        <div className="border-b-2 border-black" />
+        <div className="*:flex ">
+          <div className="items-center gap-4 py-5 group">
+            <div className="bg-black text-[#FEE502] size-8 rounded-full text-center text-md font-semibold py-1 group-hover:bg-white">
+              V
+            </div>
+            <div className="font-bold text-4xl group-hover:text-white">
+              Vancouver
+            </div>
+          </div>
+          <div className="items-center gap-4 py-5 group">
+            <div className="bg-black text-[#FEE502] size-8 rounded-full text-center text-md font-semibold py-1 group-hover:bg-white">
+              S
+            </div>
+            <div className="font-bold text-4xl group-hover:text-white">
+              Seattle
+            </div>
+          </div>
+          <div className="items-center gap-4 py-5 group">
+            <div className="bg-black text-[#FEE502] size-8 rounded-full text-center text-md font-semibold py-1 group-hover:bg-white">
+              P
+            </div>
+            <div className="font-bold text-4xl group-hover:text-white">
+              Portland
+            </div>
+          </div>
+          <div className="items-center gap-4 py-5 group">
+            <div className="bg-black text-[#FEE502] size-8 rounded-full text-center text-md font-semibold py-1 group-hover:bg-white">
+              S
+            </div>
+            <div className="font-bold text-4xl group-hover:text-white">
+              Seattle
+            </div>
+          </div>
+          <div className="items-center gap-4 py-5 group">
+            <div className="bg-black text-[#FEE502] size-8 rounded-full text-center text-md font-semibold py-1 group-hover:bg-white">
+              S
+            </div>
+            <div className="font-bold text-4xl group-hover:text-white">
+              San Francisco
+            </div>
+          </div>
+          <div className="items-center gap-4 py-5 group">
+            <div className="bg-black text-[#FEE502] size-8 rounded-full text-center text-md font-semibold py-1 group-hover:bg-white">
+              L
+            </div>
+            <div className="font-bold text-4xl group-hover:text-white">
+              Los Angeles
+            </div>
+          </div>
+          <div className="items-center gap-4 py-5 group">
+            <div className="bg-black text-[#FEE502] size-8 rounded-full text-center text-md font-semibold py-1 group-hover:bg-white">
+              S
+            </div>
+            <div className="font-bold text-4xl group-hover:text-white">
+              San Diego
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CodeChallenge;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import CodeChallenge from "./learnTailwind/CodeChallenge";
 import FormCSS from "./learnTailwind/FormCSS";
 import GroupModifiers from "./learnTailwind/GroupModifiers";
 import Input from "./learnTailwind/Input";
@@ -8,12 +9,14 @@ export default function Home() {
   return (
     // responsive
     // <main className="bg-gray-100 h-screen flex items-center justify-center p-5 sm:bg-red-100 md:bg-green-100 lg:bg-blue-100 xl:bg-orange-100 2xl:bg-purple-100 ">
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
+    // <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
+    <main>
       {/* <Input /> */}
       {/* <FormCSS /> */}
       {/* <StateModifiers /> */}
       {/* <ListAndAnimation /> */}
-      <GroupModifiers />
+      {/* <GroupModifiers /> */}
+      <CodeChallenge />
     </main>
   );
 }
